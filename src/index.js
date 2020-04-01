2c29ed7f73117a443a4bc839b7f4f98ebe9734cb
v.ignatenko <v.ignatenko360@gmail.com>
2020-04-01 16:22:19 +0300

    start

---
+++ src/index.js
@@ -0,0 +1,19 @@
+// JS
+// import "./js/";
+
+// SASS
+import "./assets/sass/main.sass";
+
+// CSS (example)
+// import './assets/css/main.css'
+
+// // Vue.js
+// window.Vue = require("vue");
+
+// // Vue components (for use in html)
+// Vue.component("example-component", require("./components/Example.vue").default);
+
+// // Vue init
+// const app = new Vue({
+//   el: "#app"
+// });
