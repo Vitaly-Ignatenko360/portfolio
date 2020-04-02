3680ba4fa0a577c52780b038d04ea2577dea3782
v.ignatenko <v.ignatenko360@gmail.com>
2020-04-02 16:34:48 +0300

    filter

--- src/js/index.js
+++ src/js/index.js
@@ -5,3 +5,18 @@ document.getElementById('menu__mobill').onclick = function() {
 document.getElementById('menu__mobill_cancel').onclick = function() {
   document.getElementById('menu__content').classList.remove('menu__content_active')
 }
+
+var filter_select_el = document.getElementById('filter');
+var items_el = document.getElementById('items');
+
+filter_select_el.onchange = function() {
+	console.log(this.value);
+  var items = items_el.getElementsByClassName('item');
+  for (var i=0; i<items.length; i++) {
+  	if (items[i].classList.contains(this.value)) {
+    	items[i].style.display = 'block';
+    } else {
+    	items[i].style.display = 'none';
+    }
+  }
+};
